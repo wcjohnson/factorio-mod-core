ce363562cd531db6a0bc6cb8bdab81a6f06f0e62
William C. Johnson <wcjohnson@oigroup.net>
2025-10-24 01:19:34 -0700

    feat: Add typing for remote callbacks

---
+++ remote-callback.lua
@@ -0,0 +1,10 @@
+-- Tools for calling back over remote interfaces.
+
+local lib = {}
+
+---Specification for the remote interface function to call back to.
+---[1] is the remote interface name.
+---[2] is the function name within that interface.
+---@alias Core.RemoteCallbackSpec [string, string]
+
+return lib
