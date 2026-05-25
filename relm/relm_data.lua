58cc86b2afb64b628aca3cf437346a287ae3517b
William C. Johnson <wcjohnson@oigroup.net>
2026-05-25 09:51:32 -0700

    feat!(relm): New data phase

--- relm/relm_data.lua
+++ relm/relm_data.lua
@@ -1,13 +1,30 @@
--- Global styles required for Relm-based mods. Some of these come from flib and
--- in those case they are identical data with the same names so as not to
--- conflict.
--- This MUST be required in the Data Phase only!
+---@diagnostic disable: undefined-global
+
+--------------------------------------------------------------------------------
+-- RELM DATA PHASE
+-- This file adds sprites and styles used by Ultros. Any Relm
+-- mod using Ultros UI controls should require this file in the data stage.
+--
+-- Embedding consumers must set the global variable `_G.__RELM_GRAPHICS_PATH__`
+-- to the subdir within their mod path where the Relm graphics files are
+-- located.
+--
+-- Some of the code below comes from `flib` under the MIT License.
+--------------------------------------------------------------------------------
 
 local styles = data.raw["gui-style"].default
 
--- SLOT BUTTON STYLES
+-- Paths
+local png_slot_tileset = _G.__RELM_GRAPHICS_PATH__ .. "slots.png"
+local png_subheader_line = _G.__RELM_GRAPHICS_PATH__ .. "subheader-line.png"
+local png_frame_action_icons = _G.__RELM_GRAPHICS_PATH__
+	.. "frame-action-icons.png"
+local png_indicators = _G.__RELM_GRAPHICS_PATH__ .. "indicators.png"
+local png_dark_red_button = _G.__RELM_GRAPHICS_PATH__ .. "dark-red-button.png"
 
-local slot_tileset = _G.__RELM_GRAPHICS_PATH__ .. "slots.png"
+--------------------------------------------------------------------------------
+-- SLOT BUTTONS (from flib)
+--------------------------------------------------------------------------------
 
 local function gen_slot(x, y, default_offset)
 	default_offset = default_offset or 0
@@ -21,7 +38,7 @@ local function gen_slot(x, y, default_offset)
 				border = 4,
 				position = { x + default_offset, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 		},
 		hovered_graphical_set = {
@@ -29,7 +46,7 @@ local function gen_slot(x, y, default_offset)
 				border = 4,
 				position = { x + 80, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 		},
 		clicked_graphical_set = {
@@ -37,7 +54,7 @@ local function gen_slot(x, y, default_offset)
 				border = 4,
 				position = { x + 160, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 		},
 		disabled_graphical_set = { -- identical to default graphical set
@@ -45,7 +62,7 @@ local function gen_slot(x, y, default_offset)
 				border = 4,
 				position = { x + default_offset, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 		},
 	}
@@ -63,7 +80,7 @@ local function gen_slot_button(x, y, default_offset, glow)
 				border = 4,
 				position = { x + default_offset, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_2_rounded_corners_glow(default_dirt_color),
 		},
@@ -72,7 +89,7 @@ local function gen_slot_button(x, y, default_offset, glow)
 				border = 4,
 				position = { x + 80, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_2_rounded_corners_glow(default_dirt_color),
 			glow = offset_by_2_rounded_corners_glow(glow),
@@ -82,7 +99,7 @@ local function gen_slot_button(x, y, default_offset, glow)
 				border = 4,
 				position = { x + 160, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_2_rounded_corners_glow(default_dirt_color),
 		},
@@ -91,7 +108,7 @@ local function gen_slot_button(x, y, default_offset, glow)
 				border = 4,
 				position = { x + default_offset, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_2_rounded_corners_glow(default_dirt_color),
 		},
@@ -110,7 +127,7 @@ local function gen_standalone_slot_button(x, y, default_offset)
 				border = 4,
 				position = { x + default_offset, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_4_rounded_corners_shallow_inset,
 		},
@@ -119,7 +136,7 @@ local function gen_standalone_slot_button(x, y, default_offset)
 				border = 4,
 				position = { x + 80, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_4_rounded_corners_shallow_inset,
 		},
@@ -128,7 +145,7 @@ local function gen_standalone_slot_button(x, y, default_offset)
 				border = 4,
 				position = { x + 160, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_4_rounded_corners_shallow_inset,
 		},
@@ -137,7 +154,7 @@ local function gen_standalone_slot_button(x, y, default_offset)
 				border = 4,
 				position = { x + default_offset, y },
 				size = 80,
-				filename = slot_tileset,
+				filename = png_slot_tileset,
 			},
 			shadow = offset_by_4_rounded_corners_shallow_inset,
 		},
@@ -158,21 +175,108 @@ local slot_data = {
 }
 
 for _, data in pairs(slot_data) do
-	styles["flib_slot_" .. data.name] = gen_slot(0, data.y)
-	styles["flib_selected_slot_" .. data.name] = gen_slot(0, data.y, 80)
-	styles["flib_slot_button_" .. data.name] =
+	styles["relm_slot_" .. data.name] = gen_slot(0, data.y)
+	styles["relm_selected_slot_" .. data.name] = gen_slot(0, data.y, 80)
+	styles["relm_slot_button_" .. data.name] =
 		gen_slot_button(240, data.y, 0, data.glow)
-	styles["flib_selected_slot_button_" .. data.name] =
+	styles["relm_selected_slot_button_" .. data.name] =
 		gen_slot_button(240, data.y, 80, data.glow)
-	styles["flib_standalone_slot_button_" .. data.name] =
+	styles["relm_standalone_slot_button_" .. data.name] =
 		gen_standalone_slot_button(240, data.y)
-	styles["flib_selected_standalone_slot_button_" .. data.name] =
+	styles["relm_selected_standalone_slot_button_" .. data.name] =
 		gen_standalone_slot_button(240, data.y, 80)
 end
 
+--------------------------------------------------------------------------------
+-- INDICATORS (from flib)
+--------------------------------------------------------------------------------
+
+local indicators = {}
+for i, color in ipairs({
+	"black",
+	"white",
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"cyan",
+	"blue",
+	"purple",
+	"pink",
+}) do
+	indicators[i] = {
+		type = "sprite",
+		name = "relm_indicator_" .. color,
+		filename = png_indicators,
+		y = (i - 1) * 32,
+		size = 32,
+		flags = { "icon" },
+	}
+end
+data:extend(indicators)
+
+--------------------------------------------------------------------------------
+-- FRAME ACTION BUTTONS (from flib)
+--------------------------------------------------------------------------------
+
+data:extend({
+	{
+		type = "sprite",
+		name = "relm_pin_black",
+		filename = png_frame_action_icons,
+		position = { 0, 0 },
+		size = 32,
+		flags = { "gui-icon" },
+	},
+	{
+		type = "sprite",
+		name = "relm_pin_white",
+		filename = png_frame_action_icons,
+		position = { 32, 0 },
+		size = 32,
+		flags = { "gui-icon" },
+	},
+	{
+		type = "sprite",
+		name = "relm_pin_disabled",
+		filename = png_frame_action_icons,
+		position = { 64, 0 },
+		size = 32,
+		flags = { "gui-icon" },
+	},
+	{
+		type = "sprite",
+		name = "relm_settings_black",
+		filename = png_frame_action_icons,
+		position = { 0, 32 },
+		size = 32,
+		flags = { "gui-icon" },
+	},
+	{
+		type = "sprite",
+		name = "relm_settings_white",
+		filename = png_frame_action_icons,
+		position = { 32, 32 },
+		size = 32,
+		flags = { "gui-icon" },
+	},
+	{
+		type = "sprite",
+		name = "relm_settings_disabled",
+		filename = png_frame_action_icons,
+		position = { 64, 32 },
+		size = 32,
+		flags = { "gui-icon" },
+	},
+})
+
+--------------------------------------------------------------------------------
+-- FLIB GENERAL STYLES (from flib)
+--------------------------------------------------------------------------------
+
 -- BUTTON STYLES
 
-styles.flib_selected_frame_action_button = {
+styles.relm_selected_frame_action_button = {
 	type = "button_style",
 	parent = "frame_action_button",
 	default_font_color = button_hovered_font_color,
@@ -198,7 +302,7 @@ styles.flib_selected_frame_action_button = {
 
 local btn = styles.button
 
-styles.flib_selected_tool_button = {
+styles.relm_selected_tool_button = {
 	type = "button_style",
 	parent = "tool_button",
 	default_font_color = btn.selected_font_color,
@@ -213,7 +317,7 @@ styles.flib_selected_tool_button = {
 	clicked_vertical_offset = 0,
 }
 
-styles.flib_tool_button_light_green = {
+styles.relm_tool_button_light_green = {
 	type = "button_style",
 	parent = "item_and_count_select_confirm",
 	padding = 2,
@@ -221,33 +325,63 @@ styles.flib_tool_button_light_green = {
 	tooltip = "",
 }
 
+styles.relm_tool_button_dark_red = {
+	type = "button_style",
+	parent = "tool_button",
+	default_graphical_set = {
+		base = {
+			filename = png_dark_red_button,
+			position = { 0, 0 },
+			corner_size = 8,
+		},
+		shadow = default_dirt,
+	},
+	hovered_graphical_set = {
+		base = {
+			filename = png_dark_red_button,
+			position = { 17, 0 },
+			corner_size = 8,
+		},
+		shadow = default_dirt,
+		glow = default_glow({ 236, 130, 130, 127 }, 0.5),
+	},
+	clicked_graphical_set = {
+		base = {
+			filename = png_dark_red_button,
+			position = { 34, 0 },
+			corner_size = 8,
+		},
+		shadow = default_dirt,
+	},
+}
+
 -- EMPTY-WIDGET STYLES
 
-styles.flib_dialog_footer_drag_handle = {
+styles.relm_dialog_footer_drag_handle = {
 	type = "empty_widget_style",
 	parent = "draggable_space",
 	height = 32,
 	horizontally_stretchable = "on",
 }
 
-styles.flib_dialog_footer_drag_handle_no_right = {
+styles.relm_dialog_footer_drag_handle_no_right = {
 	type = "empty_widget_style",
-	parent = "flib_dialog_footer_drag_handle",
+	parent = "relm_dialog_footer_drag_handle",
 	right_margin = 0,
 }
 
-styles.flib_dialog_titlebar_drag_handle = {
+styles.relm_dialog_titlebar_drag_handle = {
 	type = "empty_widget_style",
-	parent = "flib_titlebar_drag_handle",
+	parent = "relm_titlebar_drag_handle",
 	right_margin = 0,
 }
 
-styles.flib_horizontal_pusher = {
+styles.relm_horizontal_pusher = {
 	type = "empty_widget_style",
 	horizontally_stretchable = "on",
 }
 
-styles.flib_titlebar_drag_handle = {
+styles.relm_titlebar_drag_handle = {
 	type = "empty_widget_style",
 	parent = "draggable_space",
 	left_margin = 4,
@@ -256,26 +390,26 @@ styles.flib_titlebar_drag_handle = {
 	horizontally_stretchable = "on",
 }
 
-styles.flib_vertical_pusher = {
+styles.relm_vertical_pusher = {
 	type = "empty_widget_style",
 	vertically_stretchable = "on",
 }
 
 -- FLOW STYLES
 
-styles.flib_indicator_flow = {
+styles.relm_indicator_flow = {
 	type = "horizontal_flow_style",
 	vertical_align = "center",
 }
 
-styles.flib_titlebar_flow = {
+styles.relm_titlebar_flow = {
 	type = "horizontal_flow_style",
 	horizontal_spacing = 8,
 }
 
 -- FRAME STYLES
 
-styles.flib_shallow_frame_in_shallow_frame = {
+styles.relm_shallow_frame_in_shallow_frame = {
 	type = "frame_style",
 	parent = "frame",
 	padding = 0,
@@ -296,7 +430,7 @@ styles.flib_shallow_frame_in_shallow_frame = {
 
 -- IMAGE STYLES
 
-styles.flib_indicator = {
+styles.relm_indicator = {
 	type = "image_style",
 	size = 16,
 	stretch_image_to_widget_size = true,
@@ -304,7 +438,7 @@ styles.flib_indicator = {
 
 -- LABEL STYLES
 
-styles.flib_frame_title = {
+styles.relm_frame_title = {
 	type = "label_style",
 	parent = "frame_title",
 	bottom_padding = 3,
@@ -313,7 +447,7 @@ styles.flib_frame_title = {
 
 -- LINE STYLES
 
-styles.flib_subheader_horizontal_line = {
+styles.relm_subheader_horizontal_line = {
 	type = "line_style",
 	horizontally_stretchable = "on",
 	left_margin = -8,
@@ -322,14 +456,11 @@ styles.flib_subheader_horizontal_line = {
 	bottom_margin = -2,
 	border = {
 		border_width = 8,
-		horizontal_line = {
-			filename = _G.__RELM_GRAPHICS_PATH__ .. "subheader-line.png",
-			size = { 1, 8 },
-		},
+		horizontal_line = { filename = png_subheader_line, size = { 1, 8 } },
 	},
 }
 
-styles.flib_titlebar_separator_line = {
+styles.relm_titlebar_separator_line = {
 	type = "line_style",
 	top_margin = -2,
 	bottom_margin = 2,
@@ -337,7 +468,7 @@ styles.flib_titlebar_separator_line = {
 
 -- SCROLL-PANE STYLES
 
-styles.flib_naked_scroll_pane = {
+styles.relm_naked_scroll_pane = {
 	type = "scroll_pane_style",
 	extra_padding_when_activated = 0,
 	padding = 12,
@@ -346,9 +477,9 @@ styles.flib_naked_scroll_pane = {
 	},
 }
 
-styles.flib_naked_scroll_pane_under_tabs = {
+styles.relm_naked_scroll_pane_under_tabs = {
 	type = "scroll_pane_style",
-	parent = "flib_naked_scroll_pane",
+	parent = "relm_naked_scroll_pane",
 	graphical_set = {
 		base = {
 			top = { position = { 93, 0 }, size = { 1, 8 } },
@@ -358,13 +489,13 @@ styles.flib_naked_scroll_pane_under_tabs = {
 	},
 }
 
-styles.flib_naked_scroll_pane_no_padding = {
+styles.relm_naked_scroll_pane_no_padding = {
 	type = "scroll_pane_style",
-	parent = "flib_naked_scroll_pane",
+	parent = "relm_naked_scroll_pane",
 	padding = 0,
 }
 
-styles.flib_shallow_scroll_pane = {
+styles.relm_shallow_scroll_pane = {
 	type = "scroll_pane_style",
 	padding = 0,
 	graphical_set = {
@@ -375,7 +506,7 @@ styles.flib_shallow_scroll_pane = {
 
 -- TABBED PANE STYLES
 
-styles.flib_tabbed_pane_with_no_padding = {
+styles.relm_tabbed_pane_with_no_padding = {
 	type = "tabbed_pane_style",
 	tab_content_frame = {
 		type = "frame_style",
@@ -396,25 +527,28 @@ styles.flib_tabbed_pane_with_no_padding = {
 
 -- TEXTFIELD STYLES
 
-styles.flib_widthless_textfield = {
+styles.relm_widthless_textfield = {
 	type = "textbox_style",
 	width = 0,
 }
 
-styles.flib_widthless_invalid_textfield = {
+styles.relm_widthless_invalid_textfield = {
 	type = "textbox_style",
 	parent = "invalid_value_textfield",
 	width = 0,
 }
 
-styles.flib_titlebar_search_textfield = {
+styles.relm_titlebar_search_textfield = {
 	type = "textbox_style",
 	top_margin = -2,
 	bottom_margin = 1,
 	width = 150,
 }
 
--- RELM STYLES
+--------------------------------------------------------------------------------
+-- RELM GENERAL STYLES
+--------------------------------------------------------------------------------
+
 styles.relm_table_white_lines = {
 	type = "table_style",
 	horizontal_line_color = { 1, 1, 1 },
