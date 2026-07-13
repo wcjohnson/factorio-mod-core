50dd2d0bba121f7094ee77a95e8ecbb7e9bfd2bc
William C. Johnson <wcjohnson@oigroup.net>
2026-07-12 21:09:09 -0700

    fix: era counter fixes

--- math/era-counter.lua
+++ math/era-counter.lua
@@ -1,22 +1,35 @@
 -- Exponential Rolling Average counters
 
-local DEFAULT_ALPHA = 0.40
+local min = math.min
+local max = math.max
+
+local DEFAULT_ALPHA = 0.30
 
 local lib = {}
 
 ---@class Core.EraCounter
 ---@field [1] number Current ERA value
 ---@field [2] number Alpha value for the exponential rolling average
----@field [3] number One-minus-alpha value for the exponential rolling average
+---@field [3] number Min seen value
+---@field [4] number Max seen value
 
+---@param x0 number Initial value for the counter
+---@param alpha? number Alpha value for the exponential rolling average
 ---@return Core.EraCounter
-function lib.create_era_counter(alpha, x0)
-	return { x0 or 0, alpha or DEFAULT_ALPHA, 1 - (alpha or DEFAULT_ALPHA) }
+function lib.create_era_counter(x0, alpha)
+	return { x0 or 0, alpha or DEFAULT_ALPHA, x0 or 0, x0 or 0 }
 end
 
+---@param counter Core.EraCounter
+---@param x number New value to incorporate into the counter
 ---@return number
 function lib.update_era_counter(counter, x)
-	local next = counter[2] * x + counter[3] * counter[1]
+	local alpha = counter[2] or DEFAULT_ALPHA
+	local next = alpha * x + (1 - alpha) * counter[1]
 	counter[1] = next
+	counter[3] = min(counter[3], x)
+	counter[4] = max(counter[4], x)
 	return next
 end
+
+return lib
